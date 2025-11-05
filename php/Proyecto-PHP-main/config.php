69f4319edecd1496e03201c2d5a57e560e4095ef
victorfm03 <xvfremor828@ieshnosmachado.org>
2025-11-05 19:18:35 +0100

    Auto save changes

---
+++ php/Proyecto-PHP-main/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "db";
+$usuario = "nombreTest";
+$password = "test"; // o tu contraseña de XAMPP/MAMP/WAMP
+$base_de_datos = "nova_vibe";
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$base_de_datos;charset=utf8", $usuario, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
